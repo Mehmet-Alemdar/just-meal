1e7895960e76414ee749b23ca9ea61d6f97afa9b
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2022-10-23 15:22:34 +0300

    Update category card

--- components/Category.jsx
+++ components/Category.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 
 const Category = ({ data }) => (
-  <div className="md:max-w-md my-10 mx-5 text-center bg-white rounded drop-shadow-xl">
+  <div className="lg:w-80 md:w-full my-10 mx-5 text-center bg-white rounded drop-shadow-xl">
     <div className='text-center my-3'>
       <h1 className="font-normal text-lg font-serif">{ data.strCategory }</h1>
     </div>
@@ -13,7 +13,7 @@ const Category = ({ data }) => (
     />
     <div className='my-5 mx-10 text-left'>
       <p className="text-mblack font-light text-base font-sans">
-        { data.strCategoryDescription?.substring(0,400) }
+        { data.strCategoryDescription?.substring(0,250) }
         {data.strCategoryDescription?.length > 400 && <span className='text-myellow cursor-pointer font-medium'> ...read more</span>}
       </p>
     </div>
