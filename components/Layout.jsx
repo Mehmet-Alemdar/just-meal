0e6503c17dc1c9dc41db32c40f81edea23a19097
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2022-10-27 20:03:35 +0300

    Create search bar

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,19 +1,26 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import SidebarMenu from "./SidebarMenu";
+import Search from "./Searchbar";
 import { useState } from "react";
 
 
 const Layout = ({ children, filterToggle }) => {
   const [toggleSidebar, setToggleSidebar] = useState(false)
+  const [toggleSearch, setToggleSearch] = useState(false)
 
   const handleToggle = () => {
     setToggleSidebar(!toggleSidebar)
   }
 
+  const handleSearch = () => {
+    setToggleSearch(!toggleSearch)
+  }
+
   return (
     <div className="bg-mwhite">
-      <Navbar handleToggle={ handleToggle } toggleSidebar= { toggleSidebar } />
+      <Navbar handleToggle={ handleToggle } toggleSidebar= { toggleSidebar } handleSearch= { handleSearch } />
+      <Search handleSearch= { handleSearch } toggleSearch= { toggleSearch } />
       <SidebarMenu toggleSidebar={ toggleSidebar } />
       {!toggleSidebar && 
         <main>{ children }</main>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { AiOutlineClose } from 'react-icons/ai'
+import { BiSearchAlt2 } from 'react-icons/bi'
 import SidebarMenu from './SidebarMenu';
 
-const Navbar = ({ handleToggle, toggleSidebar }) => {
+const Navbar = ({ handleToggle, toggleSidebar, handleSearch }) => {
   return (
     <div className="bg-myellow flex flex-row px-10 pt-5 pb-3">
-      <div className='flex flex-row flex-none w-72  items-center gap-5'>
+      <div className='flex flex-row md:flex-1 items-center gap-5'>
         <div>
           {
             toggleSidebar 
@@ -24,12 +25,15 @@ const Navbar = ({ handleToggle, toggleSidebar }) => {
         <Link href='/'>
           <a className="px-5 text-white font-medium cursor-pointer">Home</a>
         </Link>
-        <Link href='/search'>
+        {/* <Link href='/search'>
           <a className="px-5 text-white font-medium cursor-pointer">Search</a>
-        </Link>
+        </Link> */}
         <Link href='/random-meal'>
           <a className="px-5 text-white font-medium cursor-pointer">Random Meal</a>
         </Link>
+      </div>
+      <div className='flex flex-1 flex-col justify-center items-end'>
+          <BiSearchAlt2 size={25} color="#fff" className='cursor-pointer' onClick={handleSearch}/>
       </div>
     </div>
   );
