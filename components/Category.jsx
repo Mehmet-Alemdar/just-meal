2db63e07b256346b5ad6eaf5996b05174ad62d5a
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2022-10-27 20:03:57 +0300

    Update category component

--- components/Category.jsx
+++ components/Category.jsx
@@ -1,23 +1,22 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-const Category = ({ data }) => (
-  <div className="lg:w-80 md:w-full my-10 mx-5 text-center bg-white rounded drop-shadow-xl">
-    <div className='text-center my-3'>
-      <h1 className="font-normal text-lg font-serif">{ data.strCategory }</h1>
-    </div>
-    <Image 
-      src={ data.strCategoryThumb }
-      width={320}
-      height={200}
-      alt="categoryImage"
-    />
-    <div className='my-5 mx-10 text-left'>
-      <p className="text-mblack font-light text-base font-sans">
-        { data.strCategoryDescription?.substring(0,250) }
-        {data.strCategoryDescription?.length > 400 && <span className='text-myellow cursor-pointer font-medium'> ...read more</span>}
+const Category = ({ data }) => {
+  return ( 
+    <div className="my-5 mx-5 text-center bg-white rounded drop-shadow-xl cursor-pointer hover:bg-myellow">
+        <Image 
+          src={ data.strMealThumb }
+          width={400}
+          height={400}
+          layout='responsive'
+          alt="categoryImage"
+          className="rounded-t"
+        />
+      <p className="text-mblack font-light text-base font-sans my-5 mx-10">
+        {data.strMeal}
       </p>
+
     </div>
-  </div>
-)
+   );
+}
  
 export default Category;
