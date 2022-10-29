4237bef04e9cc245e4d29c30875fc36af90448b8
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2022-10-29 23:40:42 +0300

    Update filter page

--- pages/filter/[name].js
+++ pages/filter/[name].js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import Category from "../../components/Category"
 import { baseUrl, fetchApiWithParam } from '../../utils/fetchApi'
 
@@ -9,13 +9,9 @@ const Filter = ({ meals }) => {
 
   const [showCount, setShowCount] = useState(1)
 
-  useEffect(() => {
-    console.log(showCount)
-  }, [showCount])
-
   return (
     <div className='w-ful flex flex-col gap-5 justify-center items-center text-center pt-10'>
-      <div className="w-11/12">
+      <div className="md:w-11/12">
         <h1 className='text-2xl font-light font-serif text-mblack'>{ name }</h1>
         <div className="flex flex-row">
           <div className="basis-full grid 2xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 justify-center">
@@ -34,7 +30,7 @@ const Filter = ({ meals }) => {
       </div>
       {
         meals?.length > showCount * 10 &&
-        <div className="border border-black w-72 cursor-pointer py-1 rounded hover:bg-myellow" onClick={() => setShowCount(showCount + 1)}>
+        <div className="border border-black w-72 cursor-pointer py-1 mb-10 rounded hover:bg-myellow" onClick={() => setShowCount(showCount + 1)}>
           <p>Show More</p>
         </div>
       }
