01efb1ecf579ae5986f79cd90e83dcc0cdb4c697
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2022-10-29 14:29:10 +0300

    Update Category component

--- components/Category.jsx
+++ components/Category.jsx
@@ -1,21 +1,31 @@
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router"
+
 
 const Category = ({ data }) => {
+  const router = useRouter()
+  const id = data.idMeal
+
   return ( 
-    <div className="my-5 mx-5 text-center bg-white rounded drop-shadow-xl cursor-pointer hover:bg-myellow">
+    <Link href={`/detail/${id}`}>
+        <div className="my-5 mx-5 max-w-96 text-center bg-white rounded drop-shadow-xl cursor-pointer hover:bg-myellow">
         <Image 
           src={ data.strMealThumb }
+          placeholder="blur"
+          blurDataURL={ data.strMealThumb }
           width={400}
           height={400}
           layout='responsive'
           alt="categoryImage"
           className="rounded-t"
         />
-      <p className="text-mblack font-light text-base font-sans my-5 mx-10">
-        {data.strMeal}
-      </p>
+        <p className="text-mblack font-bold text-base font-sans my-5 mx-10">{data.strMeal}</p>
+        <p className="text-mblack font-light text-base font-sans my-5 mx-10">{data?.strCategory}</p>
+        <p className="text-mblack font-light text-base font-sans my-5 mx-10">{data?.strArea}</p>
+      </div>
+    </Link>
 
-    </div>
    );
 }
  
