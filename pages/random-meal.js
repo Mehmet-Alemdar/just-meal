c944a9d172170468ad80fef04c90b7a4425a655e
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2022-10-29 20:55:10 +0300

    Deleting unnecessary codes in random meal component

--- pages/random-meal.js
+++ pages/random-meal.js
@@ -2,48 +2,6 @@ import Category from "../components/Category";
 import { baseUrl ,fetchApi} from '../utils/fetchApi'
 
 const RandomMeal = ({ randomMeals }) => {
-  const categories = [
-    {
-      strMeal: 'Baked salmon with fennel & tomatoes',
-      strMealThumb: 'https://www.themealdb.com/images/media/meals/1548772327.jpg'
-    },
-    {
-      strMeal: 'Cajun spiced fish tacos',
-      strMealThumb: 'https://www.themealdb.com/images/media/meals/uvuyxu1503067369.jpg'
-    },
-    {
-      strMeal: 'Fish pie',
-      strMealThumb: 'https://www.themealdb.com/images/media/meals/ysxwuq1487323065.jpg'
-    },
-    {
-      strMeal: 'Escovitch Fish',
-      strMealThumb: 'https://www.themealdb.com/images/media/meals/1520084413.jpg'
-    },
-    {
-      strMeal: 'Baked salmon with fennel & tomatoes',
-      strMealThumb: 'https://www.themealdb.com/images/media/meals/1548772327.jpg'
-    },
-    {
-      strMeal: 'Cajun spiced fish tacos',
-      strMealThumb: 'https://www.themealdb.com/images/media/meals/uvuyxu1503067369.jpg'
-    },
-    {
-      strMeal: 'Fish pie',
-      strMealThumb: 'https://www.themealdb.com/images/media/meals/ysxwuq1487323065.jpg'
-    },
-    {
-      strMeal: 'Escovitch Fish',
-      strMealThumb: 'https://www.themealdb.com/images/media/meals/1520084413.jpg'
-    },
-    {
-      strMeal: 'Baked salmon with fennel & tomatoes',
-      strMealThumb: 'https://www.themealdb.com/images/media/meals/1548772327.jpg'
-    },
-    {
-      strMeal: 'Cajun spiced fish tacos',
-      strMealThumb: 'https://www.themealdb.com/images/media/meals/uvuyxu1503067369.jpg'
-    }
-  ]
   return (
     <div className='w-full justify-center text-center pt-10'>
       <h1 className='text-2xl font-light font-serif text-mblack'>10 Random Meal</h1>
@@ -61,11 +19,11 @@ const RandomMeal = ({ randomMeals }) => {
 export default RandomMeal;
 
 export async function getStaticProps() {
-    const randomMeals = await fetchApi(`${baseUrl}/randomselection.php`)
+  const randomMeals = await fetchApi(`${baseUrl}/randomselection.php`)
 
-    return {
-      props: {
-        randomMeals: randomMeals.meals,
-      }
+  return {
+    props: {
+      randomMeals: randomMeals.meals,
     }
   }
+}
