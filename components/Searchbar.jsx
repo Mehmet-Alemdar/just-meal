c8d59f6ce49565643952ba6483b2ecd264ff104a
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2022-10-29 20:53:56 +0300

    Deleting unnecessary codes in searchbar component

--- components/Searchbar.jsx
+++ components/Searchbar.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { useEffect, useState } from "react";
+import { useState } from "react";
 const Search = ({ toggleSearch, handleSearch }) => {
   const [searchText, setSearchText] = useState('')
 
