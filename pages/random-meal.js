78a3823653e06801ec29e193099385522749b6f1
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2022-10-29 23:41:36 +0300

    Update random meals page

--- pages/random-meal.js
+++ pages/random-meal.js
@@ -3,13 +3,15 @@ import { baseUrl ,fetchApi} from '../utils/fetchApi'
 
 const RandomMeal = ({ randomMeals }) => {
   return (
-    <div className='w-full justify-center text-center pt-10'>
-      <h1 className='text-2xl font-light font-serif text-mblack'>10 Random Meal</h1>
-      <div className="flex flex-row">
-        <div className="basis-full grid 2xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 justify-center">
-          {randomMeals?.map((item,i) => (
-            <Category data={item} key={i} />
-          ))}
+    <div className='w-full flex justify-center text-center pt-10'>
+      <div className="md:w-11/12">
+        <h1 className='text-2xl font-light font-serif text-mblack'>10 Random Meal</h1>
+        <div className="flex flex-row">
+          <div className="basis-full grid 2xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 justify-center">
+            {randomMeals?.map((item,i) => (
+              <Category data={item} key={i} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
